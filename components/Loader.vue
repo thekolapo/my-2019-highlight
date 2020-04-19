77c08895e25874a5d0992b74b7697a10da8eea0a
Kolapo Oni <onikolapo@gmail.com>
2020-04-19 17:20:43 +0100

    chore: componentize loader section

---
+++ components/Loader.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    v-if="loadingAssets"
+    class="loader"
+  >
+    <div class="summary">
+      With the hours, days and months gone by,
+      I take a moment to reflect on the highlights
+      and happenings of 2019.
+    </div>
+    <div class="img-container">
+      <div id="loader-text">
+        {{ loaderCounter }}%
+      </div>
+      <img src="@/assets/images/loader.png" alt="loader-img">
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint semi: "error" */
+/* eslint-disable */
+import $ from 'jquery'
+import Vue from 'vue'
+
+let that = null
+
+export default {
+  data () {
+    return {
+      loadingAssets: true,
+      loaderCounter: 0,
+    }
+  },
+  mounted () {
+    that = this;
+    this.preloadImages();
+  },
+  methods: {
+    preloadImages() {
+      let images = []
+      let imgNames = [
+        'wall-of-pictures.jpg',
+        'boat-2.jpg',
+        'building-rome.jpg',
+        'chisom.jpg',
+        'cup.jpg',
+        'dominos.jpg',
+        'food-barcelona.jpg',
+        'gbems.jpg',
+        'gelato.jpg',
+        'hendricks-gin.jpg',
+        'iniye.jpg',
+        'kachi-ilashe.jpg',
+        'kolapo-ilashe.jpg',
+        'kolapo-lisbon.jpg',
+        'lara.jpg',
+        'light.jpg',
+        'tomiwa.jpg',
+        'vintage-car.jpg',
+      ]
+
+      imgNames.forEach(imgSrc => {
+        let img = new Image();
+        img.src = require(`@/assets/images/${imgSrc}`);
+        images.push(img)
+      });
+
+      imagesLoaded(images).on(
+        'progress', function( instance, image ) {
+          that.loaderCounter += 5 
+          // document.getElementById('loader-text').innerHTML = instance.progressedCount * 5;
+
+          if(that.loaderCounter == 90) {
+            that.loaderCounter = 100;
+            // document.getElementById('loader-text').innerHTML = 100;
+          }
+
+          if(that.loaderCounter == 100) {
+            window.scrollTo(0, 0);
+            new TimelineLite().to(
+              '.loader', 0.66, 
+              {
+                opacity: 0, 
+                ease: Power2.easeIn,
+                delay: 1,
+                onComplete:function(){
+                  that.loadingAssets = false;
+                }
+              }
+            )
+          }
+        }
+      );
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/sass/loader";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,6 @@
 <template>
   <div class="container">
-    <div
-      v-if="loadingAssets"
-      class="loader"
-    >
-      <div class="summary">
-        With the hours, days and months gone by,
-        I take a moment to reflect on the highlights
-        and happenings of 2019.
-      </div>
-      <div class="img-container">
-        <div id="loader-text">
-          {{ loaderCounter }}%
-        </div>
-        <img src="@/assets/images/loader.png" alt="loader-img">
-      </div>
-    </div>
+    <loader />
     <div
       id="section__hero"
       v-observe-visibility="visibilityChanged"
@@ -393,13 +378,16 @@
 import $ from 'jquery'
 import Vue from 'vue'
 import VueObserveVisibility from 'vue-observe-visibility'
+import Loader from '@/components/Loader'
 
 Vue.use(VueObserveVisibility)
 
 let that = null
 
 export default {
-  components: {},
+  components: {
+    Loader
+  },
   data () {
     return {
       visibilityThreshold: 0.2,
@@ -412,7 +400,6 @@ export default {
   },
   mounted () {
     that = this
-    this.preloadImages()
     const marquees = $('.marquee')
     const forwardDirection = [false, true, false, false, true, false, true]
 
@@ -459,62 +446,6 @@ export default {
     }, false)
   },
   methods: {
-    preloadImages () {
-      const images = []
-      const imgNames = [
-        'wall-of-pictures.jpg',
-        'boat-2.jpg',
-        'building-rome.jpg',
-        'chisom.jpg',
-        'cup.jpg',
-        'dominos.jpg',
-        'food-barcelona.jpg',
-        'gbems.jpg',
-        'gelato.jpg',
-        'hendricks-gin.jpg',
-        'iniye.jpg',
-        'kachi-ilashe.jpg',
-        'kolapo-ilashe.jpg',
-        'kolapo-lisbon.jpg',
-        'lara.jpg',
-        'light.jpg',
-        'tomiwa.jpg',
-        'vintage-car.jpg'
-      ]
-
-      imgNames.forEach((imgSrc) => {
-        const img = new Image()
-        img.src = require(`@/assets/images/${imgSrc}`)
-        images.push(img)
-      })
-
-      imagesLoaded(images).on(
-        'progress', function (instance, image) {
-          that.loaderCounter += 5
-          // document.getElementById('loader-text').innerHTML = instance.progressedCount * 5;
-
-          if (that.loaderCounter === 90) {
-            that.loaderCounter = 100
-            // document.getElementById('loader-text').innerHTML = 100;
-          }
-
-          if (that.loaderCounter === 100) {
-            window.scrollTo(0, 0)
-            new TimelineLite().to(
-              '.loader', 0.66,
-              {
-                opacity: 0,
-                ease: Power2.easeIn,
-                delay: 1,
-                onComplete () {
-                  that.loadingAssets = false
-                }
-              }
-            )
-          }
-        }
-      )
-    },
     checkIfMobileBrowser () {
       if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
         return true
